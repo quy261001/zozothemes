60953a594adc78b5d8f691ebc618316872bd3690
your name <abc@gmail.com>
2022-05-30 16:00:05 +0700

    e

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 // handle click Form 
+(function() {
 const headerIconSearch = document.querySelector(".header-icon-search");
 const headerContainer = document.querySelector(".header-container");
 const headerForm = document.querySelector(".header-form");
 const headerFormClose = document.querySelector(".header-form-icons");
 const headerbarsMb = document.querySelector(".header-bars-mobile");
 const headerbarsTb = document.querySelector(".header-bars-table");
-console.log(headerbarsTb);
 headerIconSearch.addEventListener("click", (e) => {
-  headerContainer.classList.toggle("is-noneForm");
-  headerbarsMb.classList.toggle("is-noneForm");
-  headerbarsTb.classList.toggle("is-noneForm");
+  toggleHeader([headerContainer,  headerbarsMb, headerbarsTb], "is-noneForm")
   headerForm.classList.toggle("is-showForm");
 })
 headerFormClose.addEventListener("click",() => {
-  headerContainer.classList.toggle("is-noneForm");
+  toggleHeader([headerbarsMb,headerContainer, headerbarsTb ], "is-noneForm")
   headerForm.classList.toggle("is-showForm");
-  headerbarsMb.classList.toggle("is-noneForm");
-  headerbarsTb.classList.toggle("is-noneForm");
 })
-
+function toggleHeader (eles = [], classes) {
+  eles.forEach(ele => {
+    ele.classList.toggle(classes);
+  })
+}
+})();
 // handle click Mobile 
-const menuMobileIcon = document.querySelector(".header-bars-mobile");
-const menuMobile = document.querySelector(".menu-mb");
-const wrapperMobile = document.querySelector(".wrapper ");
-const headerMobile = document.querySelector(".header ");
-
-menuMobileIcon.addEventListener("click", (e) => {
-  menuMobile.classList.toggle("is-showMenuMb");
-  wrapperMobile.classList.toggle("is-showMb");
-  headerMobile.classList.toggle("is-showMb");
-})
-
-
+(function () {
+  const menuMobileIcon = document.querySelector(".header-bars-mobile");
+  const menuMobile = document.querySelector(".menu-mb");
+  const wrapperMobile = document.querySelector(".wrapper ");
+  const headerMobile = document.querySelector(".header ");
+  menuMobileIcon.addEventListener("click", (e) => {
+    menuMobile.classList.toggle("is-showMenuMb");
+    wrapperMobile.classList.toggle("is-showMb");
+    headerMobile.classList.toggle("is-showMb");
+  })
+})();
 // handle click Menu-tb
+(function() {
 const menuTableIcon = document.querySelector(".header-bars-table");
 const menuTable = document.querySelector(".menu-tb");
 const wrapperTable = document.querySelector(".wrapper ");
@@ -40,62 +41,59 @@ const headerTable = document.querySelector(".header ");
 const optionTable = document.querySelector(".option");
 const optionIconTable = document.querySelector(".option-icon");
 menuTableIcon.addEventListener("click", (e) => {
+  MenuTb([wrapperTable, headerTable], "is-showLeftTB")
   menuTable.classList.toggle("is-showTb");
-  wrapperTable.classList.toggle("is-showLeftTB");
-  headerTable.classList.toggle("is-showLeftTB");
   optionTable.classList.toggle("is-showOptionTB");
 });
 optionIconTable.addEventListener("click", () => {
+  MenuTb([wrapperTable, headerTable], "is-showLeftTB")
   menuTable.classList.toggle("is-showTb");
-  wrapperTable.classList.toggle("is-showLeftTB");
-  headerTable.classList.toggle("is-showLeftTB");
   optionTable.classList.toggle("is-showOptionTB");
   optionTable.classList.toggle("option-show");
 });
-
+function MenuTb(elements = [], classTb) {
+  elements.forEach(element => {
+    element.classList.toggle(classTb)
+  })
+}
+})();
 // Header
-const header = document.querySelector(".header");
-const headerImg = document.querySelector(".header-img");
-const headerLink = document.querySelectorAll(".header-item-link");
-const headerIcon = document.querySelectorAll(".header-icon");
-const headerIconBars = document.querySelector(".header-icon-bars");
-const headerIconBarsMobile = document.querySelector(".header-bars-mobile");
-const formIconClose = document.querySelector(".header-form-icons");
-const headerTbImg = document.querySelector(".header-bars-table");
-const headerMbImg = document.querySelector(".header-bars-mobile");
-console.log(formIconClose);
-window.addEventListener("scroll", () => {
-  const scrollY = window.pageYOffset;
-  if (scrollY >= 100) {
-    headerLink.forEach((item) => (item.style.color = "#333"));
-    headerIcon.forEach((item) => (item.style.color = "#333"));
-    header.style.background = "#fff";
-    header.style.boxShadow = "0 0 2px rgb(0 0 0 / 10)";
-    headerImg.setAttribute(
-      "src",
-      "http://zozothemes.com/html/epoint/demo/images/logo.png"
-    );
-    formIconClose.style.color = "#000";
-    headerIconBars.style.borderColor = "#e1e1e1";
-    headerTbImg.style.color = "#000";
-    headerMbImg.style.color = "#000";
-  } else {
-    headerLink.forEach((item) => (item.style.color = "#fff"));
-    headerIcon.forEach((item) => (item.style.color = "#fff"));
-    header.style.background = "transparent";
-    headerIconBars.style.borderColor = "rgba(255,255,255,.2)";
-    headerIconBarsMobile.style.color = "#fff";
-    header.style.boxShadow = "none";
-    headerImg.setAttribute(
-      "src",
-      "http://zozothemes.com/html/epoint/demo/images/logo-light.png"
-    );
-    formIconClose.style.color = "#fff"
-    headerTbImg.style.color = "#fff";
-    headerMbImg.style.color = "#fff";
-  }
-});
-
+(function() {
+  const header = document.querySelector(".header");
+  const headerImg = document.querySelector(".header-img");
+  const headerLink = document.querySelectorAll(".header-item-link");
+  const headerIcon = document.querySelectorAll(".header-icon");
+  const headerIconBars = document.querySelector(".header-icon-bars");
+  const headerIconBarsMobile = document.querySelector(".header-bars-mobile");
+  const formIconClose = document.querySelector(".header-form-icons");
+  const headerTbImg = document.querySelector(".header-bars-table");
+  const headerMbImg = document.querySelector(".header-bars-mobile");
+  window.addEventListener("scroll", () => {
+    const scrollY = window.pageYOffset;
+    if (scrollY >= 100) {
+      headerLink.forEach((item) => (item.style.color = "#333"));
+      headerIcon.forEach((item) => (item.style.color = "#333"));
+      header.style.background = "#fff";
+      header.style.boxShadow = "0 0 2px rgb(0 0 0 / 10)";
+      headerIconBars.style.borderColor = "#e1e1e1";
+      headerImg.setAttribute("src","http://zozothemes.com/html/epoint/demo/images/logo.png");
+      headerColor([formIconClose, headerTbImg, headerMbImg], "#000")
+    } else {
+      headerLink.forEach((item) => (item.style.color = "#fff"));
+      headerIcon.forEach((item) => (item.style.color = "#fff"));
+      header.style.background = "transparent";
+      headerIconBars.style.borderColor = "rgba(255,255,255,.2)";
+      header.style.boxShadow = "none";
+      headerImg.setAttribute( "src", "http://zozothemes.com/html/epoint/demo/images/logo-light.png");
+      headerColor([formIconClose, headerTbImg, headerMbImg, headerIconBarsMobile], "#fff")
+    }
+  });
+    function headerColor(items = [], colors) {
+      items.forEach(item => {
+        item.style.color = (colors);
+      })
+    }
+})();
 // handle Click Dropdown Shopping
 const headerIconShopping = document.querySelector(".header-icon-shopping");
 const shoppingDropdown = document.querySelector(".shopping-dropdown");
@@ -103,45 +101,46 @@ headerIconShopping.addEventListener("click", (e) => {
   shoppingDropdown.classList.toggle("is-show");
   e.target.classList.toggle("blur");
 });
-
 document.addEventListener("click", (e) => {
   if (!e.target.matches(".header-icon-shopping")) {
     shoppingDropdown.classList.remove("is-show");
     headerIconShopping.classList.remove("blur");
   }
 });
-
 // handle Click Option
 const optionIcon = document.querySelector(".option-icon");
 const option = document.querySelector(".option");
 optionIcon.addEventListener("click", (e) => {
   option.classList.toggle("option-show");
 });
-
 // Handle Click Menu
-const menu = document.querySelector(".menu");
-const wrapper = document.querySelector(".wrapper");
-const menuClose = document.querySelector(".menu-close");
-headerIconBars.addEventListener("click", () => {
-  menu.classList.toggle("menu-show");
-  header.classList.toggle("menu-left");
-  wrapper.classList.toggle("menu-left");
-});
-
-menuClose.addEventListener("click", () => {
-  menu.classList.remove("menu-show");
-  header.classList.toggle("menu-left");
-  wrapper.classList.toggle("menu-left");
-});
-
-document.addEventListener("click", (e) => {
-  if (!menu.contains(e.target) && !e.target.matches(".header-icon-bars")) {
+(function() {
+  const menu = document.querySelector(".menu");
+  const wrapper = document.querySelector(".wrapper");
+  const menuClose = document.querySelector(".menu-close");
+  const headerIconBars = document.querySelector(".header-icon-bars");
+  const header = document.querySelector(".header")
+  headerIconBars.addEventListener("click", () => {
+    toggleMenu([header, wrapper], "menu-left")
+    menu.classList.toggle("menu-show");
+  });
+  menuClose.addEventListener("click", () => {
+    toggleMenu([header, wrapper], "menu-left")
     menu.classList.remove("menu-show");
-    header.classList.remove("menu-left");
-    wrapper.classList.remove("menu-left");
+  });
+  document.addEventListener("click", (e) => {
+    if (!menu.contains(e.target) && !e.target.matches(".header-icon-bars")) {
+      menu.classList.remove("menu-show");
+      header.classList.remove("menu-left");
+      wrapper.classList.remove("menu-left");
+    }
+  });
+  function toggleMenu(items = [], classTg) {
+    items.forEach((item => {
+      item.classList.toggle(classTg);
+    }))
   }
-});
-
+})();
 // slide banner-container
 const bannerWrapper = document.querySelector(".banner-wrapper");
 const bannerContainer = document.querySelector(".banner-container");
@@ -149,24 +148,23 @@ const bannerItem = document.querySelectorAll(".banner-item");
 const bannerNext = document.querySelector(".banner-next");
 const bannerPrev = document.querySelector(".banner-prev");
 const bannerDots = document.querySelectorAll(".banner-dot-item");
+  [...bannerDots].forEach((item, index) => {
+    item[index];
+    console.log(item);
+  })
 document.addEventListener("DOMContentLoaded", () => {
   banner_slide(1);
 });
 
 function banner_slide(amountSlideBanner) {
   const widthItemBanner = bannerWrapper.offsetWidth / amountSlideBanner;
-
   let widthAllBanner = widthItemBanner * bannerItem.length;
-
   bannerContainer.style.width = `${widthAllBanner}px`;
-
   bannerItem.forEach((item) => {
     item.style.width = `${widthItemBanner}px`;
   });
-
   let i = 0;
   let widthBanner = widthAllBanner - widthItemBanner * amountSlideBanner;
-
   // Next, Prev Btn
   bannerNext.addEventListener("click", () => {
     i += widthItemBanner;
@@ -174,6 +172,9 @@ function banner_slide(amountSlideBanner) {
       i = 0;
     }
     bannerContainer.style.transform = `translateX(${-i}px)`;
+    bannerDots.forEach((item, index) => {
+      item[index].classList.add("active");
+    })
   });
   bannerPrev.addEventListener("click", () => {
     i -= widthItemBanner;
@@ -181,9 +182,7 @@ function banner_slide(amountSlideBanner) {
       i = widthBanner;
     }
     bannerContainer.style.transform = `translateX(${-i}px)`;
-   
   });
-
   // DOTS
   [...bannerDots].forEach((item) => {
     item.addEventListener("click", (e) => {
